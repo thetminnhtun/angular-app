009968b82923000255aef9bd33a27b894a8fcd5f
Thet Minn Htun <thetminnhtun92@gmail.com>
2019-05-30 22:01:43 +0630

    finished Todo App

---
+++ src/app/tasks.ts
@@ -0,0 +1,5 @@
+export const tasks = [
+  {_id: 1, text: 'Learn Angular', status: 0},
+  {_id: 2, text: 'Learn React', status: 0},
+  {_id: 3, text: 'Learn Vuejs', status: 1},
+];
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { tasks } from '../tasks';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  tasks = [];
+  newTask;
+  latestId = 4;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.tasks = tasks;
+    console.log('ngOnInit');
+
+  }
+
+  doneTask(arr: any[]) {
+    return  arr.filter(item => item.status === 1);
+  }
+
+  todoTask(arr: any[]) {
+    return  arr.filter(item => item.status === 0);
+  }
+
+  add() {
+    if (this.newTask) {
+      this.tasks.push({
+        _id: this.latestId++,
+        text: this.newTask,
+        status: 0
+      });
+      this.newTask = '';
+    }
+  }
+
+  destroy(id: number) {
+    this.tasks = this.tasks.filter(item => item._id !== id);
+  }
+
+  done(id: number) {
+    this.tasks.map(item => {
+      if (item._id === id) { item.status = 1; }
+    });
+  }
+
+  undo(id: number) {
+    this.tasks.map(item => {
+      if (item._id === id) { item.status = 0; }
+    });
+  }
+
+}
